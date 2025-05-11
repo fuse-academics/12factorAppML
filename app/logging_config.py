205dc124b2e8c18d11199a8daed4408003ad9b31
AayushFuse <aayush.regmi@fusemachines.com>
2025-05-11 12:47:27 +0545

    chore: added logging config for real time logs

---
+++ app/logging_config.py
@@ -0,0 +1,11 @@
+import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(
+    level=os.getenv("LOG_LEVEL", "INFO"),
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
